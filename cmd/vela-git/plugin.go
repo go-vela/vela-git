42a665c37f66ccb38909c085bf7807cae139d370
Jordan Brockopp <Jordan.Brockopp@target.com>
2020-01-03 12:36:51 -0600

    refactor: consolidate plugin logic

--- cmd/vela-git/plugin.go
+++ cmd/vela-git/plugin.go
@@ -7,6 +7,8 @@ package main
 import (
 	"os"
 
+	"github.com/sirupsen/logrus"
+
 	"github.com/spf13/afero"
 )
 
@@ -78,6 +80,31 @@ func (p *Plugin) Exec() error {
 		if err != nil {
 			return err
 		}
+	}
+
+	return nil
+}
+
+// Validate verifies the plugin is properly configured.
+func (p *Plugin) Validate() error {
+	logrus.Debug("validating plugin configuration")
+
+	// validate build configuration
+	err := p.Build.Validate()
+	if err != nil {
+		return err
+	}
+
+	// validate netrc configuration
+	err = p.Netrc.Validate()
+	if err != nil {
+		return err
+	}
+
+	// validate repo configuration
+	err = p.Repo.Validate()
+	if err != nil {
+		return err
 	}
 
 	return nil
--- cmd/vela-git/plugin_test.go
+++ cmd/vela-git/plugin_test.go
@@ -1,1 +1,95 @@
 package main
+
+import "testing"
+
+func TestGit_Plugin_Validate(t *testing.T) {
+	// setup types
+	p := &Plugin{
+		Build: &Build{
+			Path: "/home/octocat_hello-world_1",
+			Ref:  "refs/heads/master",
+			Sha:  "7fd1a60b01f91b314f59955a4e4d4e80d8edf11d",
+		},
+		Netrc: &Netrc{
+			Machine:  "github.com",
+			Username: "octocat",
+			Password: "superSecretPassword",
+		},
+		Repo: &Repo{
+			Remote:     "https://github.com/octocat/hello-world.git",
+			Submodules: false,
+			Tags:       false,
+		},
+	}
+
+	err := p.Validate()
+	if err != nil {
+		t.Errorf("Validate returned err: %v", err)
+	}
+}
+
+func TestGit_Plugin_Validate_NoBuild(t *testing.T) {
+	// setup types
+	p := &Plugin{
+		Build: &Build{},
+		Netrc: &Netrc{
+			Machine:  "github.com",
+			Username: "octocat",
+			Password: "superSecretPassword",
+		},
+		Repo: &Repo{
+			Remote:     "https://github.com/octocat/hello-world.git",
+			Submodules: false,
+			Tags:       false,
+		},
+	}
+
+	err := p.Validate()
+	if err == nil {
+		t.Errorf("Validate should have returned err")
+	}
+}
+
+func TestGit_Plugin_Validate_NoNetrc(t *testing.T) {
+	// setup types
+	p := &Plugin{
+		Build: &Build{
+			Path: "/home/octocat_hello-world_1",
+			Ref:  "refs/heads/master",
+			Sha:  "7fd1a60b01f91b314f59955a4e4d4e80d8edf11d",
+		},
+		Netrc: &Netrc{},
+		Repo: &Repo{
+			Remote:     "https://github.com/octocat/hello-world.git",
+			Submodules: false,
+			Tags:       false,
+		},
+	}
+
+	err := p.Validate()
+	if err == nil {
+		t.Errorf("Validate should have returned err")
+	}
+}
+
+func TestGit_Plugin_Validate_NoRepo(t *testing.T) {
+	// setup types
+	p := &Plugin{
+		Build: &Build{
+			Path: "/home/octocat_hello-world_1",
+			Ref:  "refs/heads/master",
+			Sha:  "7fd1a60b01f91b314f59955a4e4d4e80d8edf11d",
+		},
+		Netrc: &Netrc{
+			Machine:  "github.com",
+			Username: "octocat",
+			Password: "superSecretPassword",
+		},
+		Repo: &Repo{},
+	}
+
+	err := p.Validate()
+	if err == nil {
+		t.Errorf("Validate should have returned err")
+	}
+}
