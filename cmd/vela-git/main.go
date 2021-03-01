bed57d56e4637f79efe496f33d5fc14e95e4edd4
Jordan <Jordan.Brockopp@target.com>
2021-03-01 08:48:29 -0600

    enhance: make version output consistent

--- cmd/vela-git/main.go
+++ cmd/vela-git/main.go
@@ -19,6 +19,19 @@ import (
 )
 
 func main() {
+	// capture application version information
+	v := version.New()
+
+	// serialize the version information as pretty JSON
+	bytes, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	// output the version information to stdout
+	fmt.Fprintf(os.Stdout, "%s\n", string(bytes))
+
+	// create new CLI application
 	app := cli.NewApp()
 
 	// Plugin Information
@@ -38,7 +51,7 @@ func main() {
 
 	app.Action = run
 	app.Compiled = time.Now()
-	app.Version = version.New().Semantic()
+	app.Version = v.Semantic()
 
 	// Plugin Flags
 
@@ -124,7 +137,7 @@ func main() {
 		},
 	}
 
-	err := app.Run(os.Args)
+	err = app.Run(os.Args)
 	if err != nil {
 		logrus.Fatal(err)
 	}
@@ -132,15 +145,6 @@ func main() {
 
 // run executes the plugin based off the configuration provided.
 func run(c *cli.Context) error {
-	// capture the version information as pretty JSON
-	v, err := json.MarshalIndent(version.New(), "", "  ")
-	if err != nil {
-		return err
-	}
-
-	// output the version information to stdout
-	fmt.Fprintf(os.Stdout, "%s\n", string(v))
-
 	// set the log level for the plugin
 	switch c.String("log.level") {
 	case "t", "trace", "Trace", "TRACE":
@@ -191,7 +195,7 @@ func run(c *cli.Context) error {
 	}
 
 	// validate the plugin
-	err = p.Validate()
+	err := p.Validate()
 	if err != nil {
 		return err
 	}
