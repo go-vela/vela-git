f62a8f77a781f5f5623eb8e26673ac7e9a0125ac
Kayla McKay <kaymckay@umich.edu>
2022-01-18 13:46:17 -0600

    chore: update copyright year to 2022

--- cmd/vela-git/main.go
+++ cmd/vela-git/main.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2021 Target Brands, Inc. All rights reserved.
+// Copyright (c) 2022 Target Brands, Inc. All rights reserved.
 //
 // Use of this source code is governed by the LICENSE file in this repository.
 
@@ -40,7 +40,7 @@ func main() {
 	app.Name = "vela-git"
 	app.HelpName = "vela-git"
 	app.Usage = "Vela Git plugin for cloning repositories"
-	app.Copyright = "Copyright (c) 2021 Target Brands, Inc. All rights reserved."
+	app.Copyright = "Copyright (c) 2022 Target Brands, Inc. All rights reserved."
 	app.Authors = []*cli.Author{
 		{
 			Name:  "Vela Admins",
