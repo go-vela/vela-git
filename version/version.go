191a0ba64fbd28323b39f96e8dde64ab3fdf1a11
wass3r <1301201+wass3r@users.noreply.github.com>
2024-08-25 01:37:09 -0500

    fix import order

--- version/version.go
+++ version/version.go
@@ -6,11 +6,10 @@ import (
 	"fmt"
 	"runtime"
 
-	"github.com/go-vela/types/version"
-
 	"github.com/Masterminds/semver/v3"
-
 	"github.com/sirupsen/logrus"
+
+	"github.com/go-vela/types/version"
 )
 
 var (
